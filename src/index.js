5e189c3cb7f1e173bcf4a955aabaf37dabb7a722
zdd0310 <54314144+zdd0310@users.noreply.github.com>
2019-12-09 17:31:02 +0800

    嘿嘿

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
-import "./assets/css/index.css"
+import "./index.css"
 import React from "react";
 import ReactDOM from "react-dom"
 import {BrowserRouter,Route,Switch,Redirect} from "react-router-dom"
 import App from "./App"
-import "./assets/css/index.css"
 
-import {mainRoutes} from "./router/routes"
+
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route path="/admin" component={App} />
-            {
-                mainRoutes.map(item=>{
-                    return <Route path={item.path} component={item.component} />
-                })
-            }
-            <Redirect to="/admin" from="/" exact />
-            <Redirect to="/404"/>
+            <Route path="/" component={App} />
+            
         </Switch>   
     </BrowserRouter>
     
