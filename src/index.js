4544dfa22621aa230f58644178f28339761990b3
zdd0310 <54314144+zdd0310@users.noreply.github.com>
2019-12-09 20:43:25 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import "./index.css"
+import "./rem"
 import React from "react";
 import ReactDOM from "react-dom"
 import {BrowserRouter,Route,Switch,Redirect} from "react-router-dom"
 import App from "./App"
+import Index from "./page/index/index"
 
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route path="/" component={App} />
-            
+            <Route path="/" component={App} exact/>
+            <Route path="/index" component={Index}/>
         </Switch>   
     </BrowserRouter>
     
