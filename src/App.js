a7221614fb8856e316e92447de6b81485e745c0a
zdd0310 <54314144+zdd0310@users.noreply.github.com>
2019-12-10 21:01:45 +0800

    跨域

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import "./App.css"
 import React,{Component} from "react"
 
 import "antd/dist/antd.css"
-import Classify from "./page/classify/Classify"
+import axios from "axios"
+
 class App extends Component{
   render(){
     return (
       <div>App</div>
     )
+  }
+
+  componentDidMount(){
+    axios("/api/goods-api/goodsByTagAlias.json?pageSize=10&page=1&alias=9rs2lr3f&offlineId=0&json=1")
+      .then(res=>{
+        console.log(res)
+      }).catch(err=>{
+        console.log(err)
+      })
   }
 }
 export default App
