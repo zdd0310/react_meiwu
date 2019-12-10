33001c054632dbd7e769289a2c3dbcba35713d69
zdd0310 <54314144+zdd0310@users.noreply.github.com>
2019-12-10 09:10:09 +0800

    2

---
+++ src/router/routes.js
@@ -0,0 +1,17 @@
+import {
+    Index,
+    Classify
+} from "../page"
+
+
+
+export const mainRoutes =[
+    {
+        path:"/index",
+        component:Index
+    },
+    {
+        path:"/classify",
+        component:Classify
+    }
+]
