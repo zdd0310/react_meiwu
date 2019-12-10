901f4e4fc50c8d345024002b12170cf8a17aee78
rousiaa <2048607407@qq.com>
2019-12-10 15:22:52 +0800

    周丹丹沙雕本屌

--- src/page/fengwei/index.js
+++ src/page/fengwei/index.js
@@ -1,10 +1,27 @@
 import React,{Component} from "react"
+import "../../assets/css/fengwei/fengwei.css"
+import Footer from "../public/footer"
 class fengwei extends Component{
     render(){
         return(
-            <div>123</div>
+            <div className="fengwei">
+                <div className="header">
+                    <a>地方风味</a>
+                    <a>水果</a>
+                    <a>甜品</a>
+                    <a>酒类</a>
+                </div>
+                <div className="content">
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+                <Footer/>
+            </div>
         )
 
     }
 }
 export default fengwei
+
+
