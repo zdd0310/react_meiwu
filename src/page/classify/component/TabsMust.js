ffc173e42d703698451b6b8232801cfe5e74ae4b
yutingting996 <53343468+yutingting996@users.noreply.github.com>
2019-12-11 19:44:47 +0800

    ytt6

--- src/page/classify/component/TabsMust.js
+++ src/page/classify/component/TabsMust.js
@@ -10,66 +10,27 @@ class TabsMust extends Component {
         console.log(state)
         return (
             <ul className="TabsMust">
-                <li className="goods" id="fumargintop">
-                    <img className="pic" src="https://img.yzcdn.cn/upload_files/2017/11/22/FrX2ClJ8SXmM0EQpSVfwBWE8UuIp.jpg!middle.jpg" />
-                    <div className="txt">
-                        <span className="title">
-                            极柚葡萄柚 约5斤/箱 水润多汁 清甜爽口
-                        </span>
-                        <p className="sub-title">
-                            极柚，一个有梦想的柚子。
-                        </p>
-                        <div className="info">
-                            <span className="price">￥69</span>
-                            <span className="buy">购买</span>
-                        </div>
-                    </div>
-                </li>
-                <li className="goods">
-                    <img className="pic" src="https://img.yzcdn.cn/upload_files/2017/11/22/FrX2ClJ8SXmM0EQpSVfwBWE8UuIp.jpg!middle.jpg" />
-                    <div className="txt">
-                        <span className="title">
-                            极柚葡萄柚 约5斤/箱 水润多汁 清甜爽口
-                        </span>
-                        <p className="sub-title">
-                            极柚，一个有梦想的柚子。
-                        </p>
-                        <div className="info">
-                            <span className="price">￥69</span>
-                            <span className="buy">购买</span>
-                        </div>
-                    </div>
-                </li>
-                <li className="goods" >
-                    <img className="pic" src="https://img.yzcdn.cn/upload_files/2017/11/22/FrX2ClJ8SXmM0EQpSVfwBWE8UuIp.jpg!middle.jpg" />
-                    <div className="txt">
-                        <span className="title">
-                            极柚葡萄柚 约5斤/箱 水润多汁 清甜爽口
-                        </span>
-                        <p className="sub-title">
-                            极柚，一个有梦想的柚子。
-                        </p>
-                        <div className="info">
-                            <span className="price">￥69</span>
-                            <span className="buy">购买</span>
-                        </div>
-                    </div>
-                </li>
-                <li className="goods" >
-                    <img className="pic" src="https://img.yzcdn.cn/upload_files/2017/11/22/FrX2ClJ8SXmM0EQpSVfwBWE8UuIp.jpg!middle.jpg" />
-                    <div className="txt">
-                        <span className="title">
-                            极柚葡萄柚 约5斤/箱 水润多汁 清甜爽口
-                        </span>
-                        <p className="sub-title">
-                            极柚，一个有梦想的柚子。
-                        </p>
-                        <div className="info">
-                            <span className="price">￥69</span>
-                            <span className="buy">购买</span>
-                        </div>
-                    </div>
-                </li>
+                {
+                    state.listData.map(item=>{
+                        return (
+                            <li className="goods" key={item.id}>
+                                <img className="pic" src={item.imageUrl} />
+                                <div className="txt">
+                                    <span className="title">
+                                        {item.title}
+                                    </span>
+                                    <p className="sub-title">
+                                        {item.subTitle}
+                                    </p>
+                                    <div className="info">
+                                        <span className="price">￥{item.price}</span>
+                                        <span className="buy">购买</span>
+                                    </div>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
             </ul>
         )
     }
@@ -77,7 +38,7 @@ class TabsMust extends Component {
 
 const mapStateProps=(state)=>{
     return {
-        state
+        state:state.MustReducer
     }
 }
 export default connect(mapStateProps,{getMustList})(TabsMust)
