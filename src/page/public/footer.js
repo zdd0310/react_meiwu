60720ca1b600a99342c06a75fdc0fb7036e5ff65
zdd0310 <54314144+zdd0310@users.noreply.github.com>
2019-12-14 16:38:43 +0800

    2

--- src/page/public/footer.js
+++ src/page/public/footer.js
@@ -1,13 +1,21 @@
-import React from "react"
 import "../../assets/css/public.css"
 import {Icon} from "antd"
-export default function Footer(){
-    return(
-        <div className="footer">
-            <p><Icon type="home" style={{fontSize:".18rem"}}/></p>
-            <a>分类</a>
-            <a>地方风味</a>
-            <a>我的</a>
-        </div>
-    )
+import {withRouter} from "react-router-dom"
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    to=(path)=>{
+        this.props.history.push(path)
+    }
+    render() {
+        return (
+            <div className="footer">
+                <p onClick={this.to.bind(this,"/index")}><Icon type="home" style={{fontSize:".18rem"}}/></p>
+                <a onClick={this.to.bind(this,"/classify")}>分类</a>
+                <a onClick={this.to.bind(this,"/fengwei")}>地方风味</a>
+                <a>我的</a>
+            </div>
+        )
+    }
 }
+export default withRouter(Footer)
