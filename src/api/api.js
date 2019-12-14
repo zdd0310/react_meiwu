c198f3991a9148d0db4e075a9ede82f8d4bd338a
zdd0310 <54314144+zdd0310@users.noreply.github.com>
2019-12-14 10:59:32 +0800

    1

--- src/api/api.js
+++ src/api/api.js
@@ -1,22 +1,52 @@
 import axios from "./axios"
 
 export const getList=()=>{
-    return axios("/showcase/goodsList.json",{
+    return axios("/showcase/goodsList.json?goodsIds=508363198%2C542140256%2C505785309%2C538603472%2C538606117%2C503967646%2C493691129%2C527323701%2C492164967%2C491976337",{
         params : {
-          tagId: 108071946,
+          tagId: "",
           page: 1,
-          pageSize: 8,
-          goodsIds: "",
-          goodsFrom: 1,
+          pageSize: 6,
+          goodsFrom: 0,
           isAdv: 0,
           offlineId: 0,
-          goodsNumber: 8,
+          goodsNumber: 6,
           json: 1,
           kdt_id:17879790 
         }
       })
 }
 
+export const getHotList=(tagId,pageSize,goodsNumber)=>{
+  return axios("/showcase/goodsList.json",{
+    params:{
+      tagId,
+      page: 1,
+      pageSize,
+      goodsIds:"",
+      goodsFrom: 1,
+      isAdv: 0,
+      offlineId: 0,
+      goodsNumber,
+      json: 1,
+      kdt_id:17879790 
+    }
+  })
+}
+export const getPeishiList=()=>{
+  return axios("/showcase/goodsList.json?goodsIds=488832511%2C426862811%2C426864459%2C316900772%2C427327270%2C488848008",{
+    params:{
+      tagId: "",
+      page: 1,
+      pageSize: 6,
+      goodsFrom: 0,
+      isAdv: 0,
+      offlineId: 0,
+      goodsNumber: 6,
+      json: 1,
+      kdt_id:17879790 
+    }
+  })
+}
 export const getClassifyMustList=()=>{
   return axios("/goods-api/goodsByTagAlias.json",{
     params:{
